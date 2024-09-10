00335e6fbf87d8ad16ec2142f7ad6ca495c6fa08
Roman Sherstiuk <ronmacdonald228@gmail.com>
2024-09-10 10:23:58 +0200

    Initial commit

---
+++ src/main/java/com/roman/decision_cloud_stream/DecisionCloudStreamApplication.java
@@ -0,0 +1,13 @@
+package com.roman.decision_cloud_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionCloudStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecisionCloudStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roman/decision_cloud_stream/DecisionCloudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roman.decision_cloud_stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionCloudStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
